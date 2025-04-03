f083407fbe2d979fa6739eaad164a549b1990e51
fray osorio <frayosorio@gmail.com>
2025-04-02 19:36:37 -0500

    Se agrega la capa de INFRAESTRUCTURA con los repositorios

---
+++ infraestructura/src/main/java/repositorios/ICampeonatoRepositorio.java
@@ -0,0 +1,14 @@
+package campeonatosfifa.api.infraestructura.repositorios;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import campeonatosfifa.api.dominio.entidades.*;
+
+public interface ICampeonatoRepositorio  extends JpaRepository<Campeonato, Integer> {
+
+    @Query("SELECT c FROM Campeonato c WHERE c.nombre LIKE '%' || ?1 || '%'")
+    public List<Campeonato> buscar(String nombre);
+
+}
