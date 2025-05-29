fae43a6c68eafa2e70c34abac06277811614f79f
fray osorio <frayosorio@gmail.com>
2025-05-28 22:30:39 -0500

    Se agrega controlador para CAMPEONATO

--- presentacion/src/main/java/controladores/SeleccionControlador.java
+++ presentacion/src/main/java/controladores/SeleccionControlador.java
@@ -7,8 +7,6 @@ import campeonatosfifa.api.dominio.entidades.*;
 import campeonatosfifa.api.core.servicios.*;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/selecciones")
@@ -17,19 +15,34 @@ public class SeleccionControlador {
     @Autowired
     private ISeleccionServicio servicio;
 
-    @RequestMapping(value="/listar", method=RequestMethod.GET)
-    public List<Seleccion> listar(){
+    @RequestMapping(value = "/listar", method = RequestMethod.GET)
+    public List<Seleccion> listar() {
         return servicio.listar();
     }
-   
+
     @GetMapping("/obtener/{id}")
-    public Seleccion obtener(@PathVariable int id){
+    public Seleccion obtener(@PathVariable int id) {
         return servicio.obtener(id);
     }
 
     @GetMapping("/buscar/{nombre}")
-    public List<Seleccion> buscar(@PathVariable String nombre){
+    public List<Seleccion> buscar(@PathVariable String nombre) {
         return servicio.buscar(nombre);
+    }
+
+    @PostMapping("/agregar")
+    public Seleccion agregar(@RequestBody Seleccion pais) {
+        return servicio.agregar(pais);
+    }
+
+    @RequestMapping(value = "/modificar", method = RequestMethod.PUT)
+    public Seleccion modificar(@RequestBody Seleccion pais) {
+        return servicio.modificar(pais);
+    }
+
+    @RequestMapping(value = "/eliminar/{id}", method = RequestMethod.DELETE)
+    public boolean eliminar(@PathVariable int id) {
+        return servicio.eliminar(id);
     }
 
 }
